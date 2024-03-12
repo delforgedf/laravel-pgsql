37c32b57fa869ca5666dba7905cdf802fee55a0b
delforgeseinfra <delforge@NOTWEB001667>
2024-03-12 14:09:51 -0300

    adicionando rotas de auth ao projeto

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth routes
+Route::get('admin/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('admin/login',  [LoginController::class, 'login']);
+Route::post('admin/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('password/reset', [ResetPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ResetPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset']);
